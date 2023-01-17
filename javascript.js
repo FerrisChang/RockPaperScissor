f24f8d2d338fc7da19faed21f117e7d98ba4c02c
Ferris Chang <103004499+FerrisChang@users.noreply.github.com>
2023-01-17 12:04:23 -0500

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -1,3 +1,4 @@
+//Randomly generates a number and then will output the results either(rock, paper, scissors)
 function getComputerChoice(){
     let choice = Math.floor(Math.random()*10);
     if(choice <= 3) {
@@ -8,8 +9,7 @@ function getComputerChoice(){
         return 'scissor';
     }
 }
-
-
+//will play one round and take results passed in and compare in if-else statements.
 function playRound(playerSelection, computerSelection){
     if(playerSelection === 'rock'){
         if(computerSelection === 'rock'){
@@ -27,7 +27,7 @@ function playRound(playerSelection, computerSelection){
         }else{
             return 'win';
         }
-    }else if(playerSelection === 'scissor'){
+    }else{
         if(computerSelection === 'scissor'){
             return 'tie';
         }else if(computerSelection === 'rock'){
@@ -35,34 +35,54 @@ function playRound(playerSelection, computerSelection){
         }else{
             return 'win';
         }
-    }else{
-        return 'invalid choice';
     }
 }
-
-
-
-function game(){
-    
-    let computerScore = 0;
-    let playerScore = 0;
-    let ties = 0;
-    
-    for(let i = 0; i < 5; i++){
-        const playerSelection = prompt("choose rock, paper, or scissor");
+//This will determine the winner of the game and append the score to the global containers
+//Added score hitting 5 and displaying the winner
+//* Will need to add a reset when the winner is found */
+//* need to get the message to show when the number actually hits 5 not past it */
+function game(playerChoice){
+        const playerSelection = playerChoice;
         const computerSelection = getComputerChoice();
-    
-        if (playRound(playerSelection, computerSelection) === 'win'){
-            playerScore += 1;
-        } else if (playRound(playerSelection, computerSelection) === 'lose') {
-            computerScore += 1;
+        if(playerScore == 5 || computerScore == 5){
+            if(playerScore == 5){
+                document.querySelector('#results-box').innerHTML = "You win!"
+            } else{
+                document.querySelector('#results-box').innerHTML = "You Lose!"
+            }
         } else {
-            ties += 1;
+            if (playRound(playerSelection, computerSelection) === 'win'){
+                playerScore += 1;
+                document.querySelector('.player-result-score').innerHTML = playerScore;
+            } else if (playRound(playerSelection, computerSelection) === 'lose') {
+                computerScore += 1;
+                document.querySelector('.computer-result-score').innerHTML = computerScore;
+            } else {
+                ties += 1;
+                document.querySelector('.tie-result-score').innerHTML = ties;        
+            }
         }
-        console.log("the computer choose " + computerSelection + " and you choose " + playerSelection + " so the result was " + playRound(playerSelection, computerSelection));
-        console.log("computer score: " + computerScore);
-        console.log("player score: " + playerScore);
-        console.log("total ties: " + ties);
-    }
+        
 }
-game();
+//Will wait for the person to hit the button then pass the result.
+const rock = document.getElementById('rock');
+rock.addEventListener('click', () => {
+    game('rock');
+});
+const paper = document.getElementById('paper');
+paper.addEventListener('click', () => {
+    game('paper');
+});
+const scissor = document.getElementById('scissor');
+scissor.addEventListener('click', () => {
+    game('scissor');
+});
+
+let computerScore = 0;
+let playerScore = 0;
+let ties = 0;
+
+/* EDITORS NOTES */
+//reformat code to add color and decorations
+//reorganize code to follow best practice
+//test for errors
